07400e57c6e2955487726806630a1f7c48997cd4
Devendra Vyas <devendra.y12@lnmiit.ac.in>
2023-04-11 14:12:41 +0200

    Create render_x_ray.sh

---
+++ graf-main/render_x_ray.sh
@@ -0,0 +1,6 @@
+python render_xray_G.py configs/knee.yaml /
+    --xray_img_path /home/paperspace/projects/mednerf/data/knee_xrays /
+    --save_dir ./renderings /
+    --model /home/paperspace/projects/mednerf/pre_trained_models/carla_64.pt /
+    --save_every 25 /
+    --psnr_stop 25 
